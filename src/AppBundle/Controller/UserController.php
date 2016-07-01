77e4dc717bba7ccb88f682c26c59dd8f3cd9ee0e
Zachariasz <zah3@o2.pl>
2016-07-01 19:27:20 +0200

    added very basic Admin Panel

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AppBundle\Entity\User;
+use AppBundle\Entity\Crew;
+use AppBundle\Entity\Event;
+
+/**
+ * Class UserController
+ * @Route("/panelAdmin")
+ */
+class UserController extends Controller
+{
+
+    /**
+     * @Route("/index")
+     * @Template("AppBundle:User:index.html.twig")
+     */
+    public function showIndexAction()
+    {
+        $events = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findEventsNotAccepted();
+
+        $crews = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findCrewsNotAccepted();
+        
+        return ['events' => $events, 'crews' => $crews];
+        
+    }
+    /**
+     * @Route("/users")
+     * @Template("AppBundle:User:showAllUsers.html.twig")
+     */
+    public function showAllUsersAction()
+    {
+        $users = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findAllUsersSorted();
+
+        return ['users' => $users];
+    }
+
+    /**
+     * @Route("/remove/{id}")
+     *
+     */
+    public function removeUserAction($id)
+    {
+        $user = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->find($id);
+
+        if(!$user){
+            throw $this
+                ->createNotFoundException('Cannot found User');
+        }
+
+        $em = $this
+            ->getDoctrine()
+            ->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        return $this->render('AppBundle:User:deleteUser.html.twig');
+    }
+    
+    /**
+     * @Route("/crews")
+     * @Template("AppBundle:User:showAllCrews.html.twig")
+     */
+    public function showAllCrewAction()
+    {
+        $crews = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findAllCrews();
+
+        return ['crews' => $crews];
+    }
+
+    /**
+     * @Route("/crew/remove/{id}")
+     *
+     */
+    public function removeCrewAction($id)
+    {
+        $crew = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:Crew')
+            ->find($id);
+
+        if(!$crew){
+            throw $this
+                ->createNotFoundException('Cannot found Crew');
+        }
+
+        $em = $this
+            ->getDoctrine()
+            ->getManager();
+        $em->remove($crew);
+        $em->flush();
+
+        return $this->render('AppBundle:User:deleteCrew.html.twig');
+    }
+
+    /**
+     * @Route("/events")
+     * @Template("AppBundle:User:showAllEvents.html.twig")
+     */
+    public function showAllEventsAction()
+    {
+        $events = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findAllEvents();
+
+        return ['events' => $events];
+    }
+
+    /**
+     * @Route("/event/remove/{id}")
+     *
+     */
+    public function removeEventAction($id)
+    {
+        $event = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:Event')
+            ->find($id);
+
+        if(!$event){
+            throw $this
+                ->createNotFoundException('Cannot found Event');
+        }
+
+        $em = $this
+            ->getDoctrine()
+            ->getManager();
+        $em->remove($event);
+        $em->flush();
+
+        return $this->render('AppBundle:User:deleteEvent.html.twig');
+    }
+    /**
+     * @Route("/crew/getAccepted/{id}")
+     */
+    public function setAcceptToCrewAction($id)
+    {
+        $crew = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:Crew')
+            ->find($id);
+        if(!$crew){
+            throw $this
+                ->createNotFoundException('Cannot found Crew.');
+        }
+        elseif($crew->getAccepted() == 1){
+            throw $this
+                ->createNotFoundException('This Crew has activated before.');
+        }
+        else{
+            $crew->setAccepted(1);
+            $em = $this
+                ->getDoctrine()
+                ->getManager();
+            $em->persist($crew);
+            $em->flush();
+
+            return $this->render('AppBundle:User:accepted.html.twig');
+        }
+
+    }
+
+    /**
+     * @Route("/event/getAccepted/{id}")
+     */
+    public function setAcceptToEventAction($id)
+    {
+        $event = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:Event')
+            ->find($id);
+        if (!$event) {
+            throw $this
+                ->createNotFoundException('Cannot found Event.');
+        } elseif ($event->getAccepted() == 1) {
+            throw $this
+                ->createNotFoundException('This Event has activated before.');
+        } else {
+            $event->setAccepted(1);
+            $em = $this
+                ->getDoctrine()
+                ->getManager();
+            $em->persist($event);
+            $em->flush();
+
+            return $this->render('AppBundle:User:accepted.html.twig');
+        }
+    }
+
+}
